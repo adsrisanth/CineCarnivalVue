1f65b9d779d07fdd1c4863f64df47c63547a45ed
Adari Dinesh Srisanth <adsrisanth2005@gmail.com>
2024-01-12 23:21:31 +0530

    final_update_4

---
+++ frontend/src/data/events.js
@@ -0,0 +1,135 @@
+export const EventsData = [
+  {
+    index: 0,
+    title: 'Short Film Contest',
+    poster: 'https://i.postimg.cc/Z4NmZC47/short-film-contest-2.png',
+    image:
+      'https://i.postimg.cc/b8s8CmMj/1-VERTICAL-2.png',
+      
+    description: 'Welcome to "Imaginary Realms" – a short film contest celebrating the limitless power of creativity! Dive into a world where imagination knows no bounds and storytelling takes center stage. Submit your 5 to 15-minute masterpiece, exploring any genre and language. Surprise us, inspire us, and let your creativity shine! Cash prizes, trophies, and recognition await the most captivating entries. ',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    type: 'InContest',
+    duration: '2 hours',
+    club_name: 'Photography Club',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: '21000',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  },
+  {
+    index: 1,
+    title: 'Reel Contest',
+    poster: 'https://i.postimg.cc/Qjr3MhMX/reel-making-contest-2.png',
+    image:
+      'https://i.postimg.cc/QXfGS7Jt/reel-making-vertical-2.png',
+    description: 'Description for Event 2',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    duration: '2 hours',
+    type: 'Contest',
+    club_name: 'Photography & Film Technology',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: 'Contact details for Event 2',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  },
+  {
+    index: 2,
+    title: 'Photography Contest',
+    poster: 'https://i.postimg.cc/pPYth49K/PHOTOGRAPHY-CONTEST.png',
+    duration: '2 hours',
+    image:
+      'https://i.postimg.cc/y1WqGNLT/PHOTOGRAPHY-VERTICAL.png',
+    description: 'Description for Event 3',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    type: 'Contest',
+    club_name: 'Photography & Film Technology',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: 'Contact details for Event 3',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  },
+  {
+    index: 3,
+    title: 'Script Witing',
+    poster: 'https://i.postimg.cc/9VK2vG5w/SCRPIT-WRITING.png',
+    image:
+      'https://i.postimg.cc/81pgh4W9/SCRIPT-WRITING-VERTICAL.png',
+    description: 'Description for Event 4',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    duration: '2 hours',
+    type: 'Contest',
+    club_name: 'Photography & Film Technology',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: 'Contact details for Event 4',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  },
+  {
+    index: 3,
+    title: 'Script Witing',
+    poster: 'https://i.postimg.cc/9VK2vG5w/SCRPIT-WRITING.png',
+    image:
+      'https://i.postimg.cc/81pgh4W9/SCRIPT-WRITING-VERTICAL.png',
+    description: 'Description for Event 4',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    duration: '2 hours',
+    type: 'Contest',
+    club_name: 'Photography & Film Technology',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: 'Contact details for Event 4',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  },
+  {
+    index: 3,
+    title: 'Script Witing',
+    poster: 'https://i.postimg.cc/9VK2vG5w/SCRPIT-WRITING.png',
+    image:
+      'https://i.postimg.cc/81pgh4W9/SCRIPT-WRITING-VERTICAL.png',
+    description: 'Description for Event 4',
+    category: 'LCH - Liberal, Creative and Hobby ',
+    duration: '2 hours',
+    type: 'Contest',
+    club_name: 'Photography & Film Technology',
+    guidelines: [
+      'Event guideline 1',
+      'Event guideline 2',
+      'Event guideline 3',
+      'Event guideline 4'
+    ],
+    team_size: 100,
+    contact: 'Contact details for Event 4',
+    date: '23rd & 24th February "2K24.',
+    register: 'https://forms.gle/NMsZpuVediCyFyxw8'
+  }
+]
---
+++ frontend/src/views/PageTwoView.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Page2Video from '../assets/video/Page2Video.mp4';
+import { ref, onMounted } from 'vue';
+
+const bgVideo2 = ref(null);
+
+onMounted(() => {
+      if (bgVideo2.value) {
+        bgVideo2.value.play().catch(error => {
+          console.error('Autoplay failed:', error);
+        });
+      }
+    })
+</script>
+
+<template>
+    <div class="app-container relative justify-start max-ssm:px-8 max-md:justify-center max-xl:justify-end flex place-items-center m-0 overflow-hidden">
+      <video ref="bgVideo2" class="object-cover" muted loop autoplay width="100%" height="100vh" :style="{ opacity: '0.5' }">
+        <source :src="Page2Video" type="video/mp4">
+      </video>
+      <div class="overlay-box md:mx-16">
+        <h2 class="overlay-heading font-extrabold mb-3 text-[40px] max-md:text-[35px] max-sm:text-[30px] max-ssm:text-[25px]">Carnival' 24</h2>
+        <p class="text-[17px] font-montserrat max-sm:text-[16px] max-ssm:text-[14px] mb-3 ">The "KL Cine Carnival" Is Being Orchestrated By The KL University's Student Activity Center,
+          Known As "KL Sac Film Technology Club." This Student-Led Organization Receives Support From
+          The Student Activity Centre (SAC) At KL University. KL Cinemalu Is A Hub For Young And
+          Aspiring Talents Who Share A Deep Passion For The World Of A Cinema And Filmmaking.<br><br>
+        </p>
+      </div>
+    </div>
+
+
+</template>
+
+<style scoped>
+.app-container {
+  height: 100vh;
+  background-color: #000;
+}
+
+.overlay-box {
+  max-width: 500px;
+  background-color: transparent;
+  backdrop-filter: blur(25px);
+  box-shadow: 0 0 10 rgba(0, 0, 0, .5);
+  padding: 20px;
+  border-radius: 10px;
+  text-align: left;
+  color: #fff;
+}
+
+.overlay-box p {
+  letter-spacing: 1px;
+}
+
+.overlay-heading {
+  font-family: 'Merriweather', serif;
+}
+
+
+video {
+  object-fit: fill;
+  position: absolute;
+  height: 100%;
+}
+
+.content-container {
+  text-align: center;
+  padding: 20px;
+  background-color: #000;
+}
+
+</style>
